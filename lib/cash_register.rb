78cfd405cf39bf42e3d8921aea6f24a3518fe48e
waselrosa <rosa@waselrosa.com>
2014-12-17 01:53:40 -0800

    TDD cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,17 @@
 class CashRegister
-  def total
-    0
+
+  attr_accessor :total
+
+  def initialize
+    @total = 0.0
   end
+
+  def purchase(price)
+    @total += price.to_f
+  end
+
+  def pay(amount_paid)
+    change = amount_paid - @total
+  end
+
 end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -10,4 +10,33 @@ describe CashRegister do
       expect(total).to eq(0)
     end
   end
+
+  describe "#purchase" do
+    it "adds to total" do
+      cash_register = CashRegister.new
+      cash_register.purchase(9.95)
+
+      expect(cash_register.total).to eq(9.95)
+    end
+
+    it "adds multiple items to total" do
+      cash_register = CashRegister.new
+      cash_register.purchase(2.95)
+      cash_register.purchase(11.42)
+      cash_register.purchase(20.99)
+      cash_register.purchase(75.00)
+
+      expect(cash_register.total).to eq(110.36)
+    end
+  end
+
+  describe "#pay" do
+    it "receives an amount and returns change" do
+      cash_register = CashRegister.new
+      cash_register.purchase(5.25)
+
+      expect(cash_register.pay(10)).to eq(4.75)
+    end
+  end
+
 end
